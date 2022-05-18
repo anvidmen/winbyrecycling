5ba919f06ed5fd3e0cf89ad597573d42ef92de5e
Juan Omen <anvidmen@gmail.com>
2022-05-18 11:32:02 +0200

    added About skeleton structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from 'pages/Home/Home';
+import About from 'pages/About/About';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' caseSensitive={false} element={<Home />} />
+        <Route path='/about' caseSensitive={false} element={<About />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/About/About.jsx
@@ -0,0 +1,11 @@
+import Body from "components/layout/Body/Body"
+
+const About = () => {
+    return (
+        <Body>
+            <h1>Nuestro objetivo, nuestro equipo</h1>
+        </Body>
+    )
+}
+
+export default About
