61e00f427794fdf4539d1feb1f0380a42deb158c
Juan Omen <anvidmen@gmail.com>
2022-05-18 11:37:33 +0200

    added Games skeleton structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from 'pages/Home/Home';
-import About from 'pages/About/About';
+import Home from 'pages/Home/Home'
+import About from 'pages/About/About'
+import Games from 'pages/Games/Games'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path='/' caseSensitive={false} element={<Home />} />
         <Route path='/about' caseSensitive={false} element={<About />} />
+        <Route path='/games' caseSensitive={false} element={<Games />} />
       </Routes>
     </Router>
   );
