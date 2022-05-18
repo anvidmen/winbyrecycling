e5963c6f10a5ca2e43e93b66421b3d828d13a395
Juan Omen <anvidmen@gmail.com>
2022-05-18 11:40:15 +0200

    added Contact skeleton structure

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from 'pages/Home/Home'
 import About from 'pages/About/About'
 import Games from 'pages/Games/Games'
+import Contact from 'pages/Contact/Contact'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path='/' caseSensitive={false} element={<Home />} />
         <Route path='/about' caseSensitive={false} element={<About />} />
         <Route path='/games' caseSensitive={false} element={<Games />} />
+        <Route path='/contact' caseSensitive={false} element={<Contact />} />
       </Routes>
     </Router>
   );
