da1d94da5df097a43d86b00025d0e813d1869e91
Juan Omen <anvidmen@gmail.com>
2022-06-08 09:55:42 +0200

    update contact form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,33 +1,99 @@
-import Body from "components/layout/Body/Body"
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import Swal from 'sweetalert2'
+import { validatedEmail, validatedName } from 'utils/validations'
+import Body from 'components/layout/Body/Body'
+import contact from 'assets/images/contact.jpeg'
+import styles from './Contact.module.scss'
 
 const Contact = () => {
     const form = useRef();
 
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm('service_yh3oiaa', 'template_5rqq7qe', form.current, 'LQ588QouS1DL8SU0s')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-    };
+    const sendEmail = async (e) => {
+        e.preventDefault();
+        let { name, email } = e.target
+        name = name.value
+        email = email.value
+
+        if (name.trim() === '' || email.trim() === '') {
+            return Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Todos los campos son obligatorios'
+            })
+        }
+
+        if (validatedName(name) === false) {
+            return Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Formato del nombre no es valido'
+            })
+        }
+
+        if (validatedEmail(email) === false) {
+            return Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Formato del email no es valido'
+            })
+        }
+
+        try {
+            await emailjs.sendForm('service_yh3oiaa', 'template_5rqq7qe', form.current, 'LQ588QouS1DL8SU0s')
+            e.target.reset();
+            return Swal.fire({
+                icon: 'success',
+                text: 'Tu mensaje a sido enviado'
+            })
+        } catch (error) {
+            return Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: error.message
+            })
+        }
+    }
+
     return (
-      <Body>
-        <h1>Contacta con nosotros</h1>
-        <form ref={form} onSubmit={sendEmail}>
-          <label>Name</label>
-          <input type="text" name="name" />
-          <label>Email</label>
-          <input type="email" name="email" />
-          <label>Message</label>
-          <textarea name="message" />
-          <input type="submit" value="Send" />
-        </form>
-      </Body>
+        <Body>
+            <div className={styles.main}>
+                <div className={styles.mainBody}>
+                    <img alt="contact" src={contact}></img>
+                </div>
+                <form ref={form} className={styles.mainForm} onSubmit={sendEmail}>
+                    <h2>Contactanos</h2>
+                    <div>
+                        <label>Nombre</label> <br />
+                        <input
+                            autoComplete="off"
+                            type="text"
+                            placeholder="Introduce tu nombre"
+                            name="name"
+                            id='name'
+                        />
+                    </div>
+                    <div className={styles.formElement}>
+                        <label>Email</label> <br />
+                        <input
+                            autoComplete="off"
+                            placeholder="Introduce tu email"
+                            name="email"
+                            id='email'
+                        />
+                    </div>
+                    <div className={styles.formElement}>
+                        <label>Mensaje</label>
+                        <textarea
+                            name="message"
+                            id='msg' rows="5"
+                            placeholder="Pregunta/deja tu comentario..."
+                        />
+                    </div>
+                    <input className={styles.submit} type="submit" value="Enviar" />
+                </form>
+            </div>
+        </Body>
     );
 }
 
