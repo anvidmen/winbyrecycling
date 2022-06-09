368de4a42bb92312e14cadb892a9c8fe9bf41fc1
Adrian Nuñez Martin <adrimart212@gmail.com>
2022-06-09 10:05:40 +0200

    build final

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = "https://winbyrecyciling.herokuapp.com"
+const baseURL = "https://winbyrecycling.herokuapp.com"
 
 export const login = data => axios.post(`${baseURL}/login`, data)
 export const register = data => axios.post(`${baseURL}/user`, data)
